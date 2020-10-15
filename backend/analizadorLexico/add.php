babf7e2a38c4a58e0a3021604f3e8006189edce1
Luis <lpachecoby@gmail.com>
2020-10-15 15:32:06 -0500

    Luis

--- backend/analizadorLexico/add.php
+++ backend/analizadorLexico/add.php
@@ -1,9 +1,13 @@
 <?php
+//Obtenemos la ruta actual.
 $url = parse_url($_SERVER['REQUEST_URI']);
 
+//validamos el elemento de la url que esté como parametro, en este caso todo lo que está despues del signo "?"
 if (empty($url['query'])) {
+    //si está vacio pedimos que se carge el archivo
     echo 'Cargar archivo';
 } else {
+    //si existe un parametro lo obtenemos con el metodo htmlspecialchars
     $nombre = htmlspecialchars($_GET["archivo"]);
 ?>
     <div class="container-fluid">
@@ -12,9 +16,11 @@ if (empty($url['query'])) {
         <div class="row">
             <div class="col">
                 <?php
+                //Incluimos las clases del analizador.
                 include("backend/analizadorLexico/stringtokenizer.class.php");
                 include("backend/analizadorLexico/lexer.class.php");
                 $txt = '';
+                
                 $fp = fopen("backend/files/" . $nombre, "r");
                 while (!feof($fp)) $txt .= fgets($fp);
                 fclose($fp);
--- backend/files/source.txt
+++
@@ -1,6 +0,0 @@
-entero = sumarRestar
-si edad > 18 entonces
-    escriba "Eres mayor de edad"
-fin
-entero nacimiento = 2014 / - edad
-escriba nacimiento
