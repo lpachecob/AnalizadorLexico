53831c311d3740894a6e26c80fc65891b28bb84d
Luis Pacheco <lpachecoby@gmail.com>
2020-10-23 17:03:13 -0500

    correccion de url

--- frontend/elementos/slidebar/slidebar.php
+++ frontend/elementos/slidebar/slidebar.php
@@ -1,14 +1,14 @@
 <ul class="navbar-nav bg-gradient-primary sidebar sidebar-dark accordion" id="accordionSidebar">
 
     <!-- Sidebar - Brand -->
-    <a class="sidebar-brand d-flex align-items-center justify-content-center" href="/AnalizadorLexico">
+    <a class="sidebar-brand d-flex align-items-center justify-content-center" href="https://lexico.lpachecob.com/">
         <div class="sidebar-brand-icon rotate-n-15">
             <i class="fas fa-users"></i>
         </div>
         <div class="sidebar-brand-text mx-3">Grupo 6</div>
     </a>
 
-    
+
 
     <!-- Divider -->
     <hr class="sidebar-divider d-none d-md-block">
